6f8dbde0f78d30487acfabf2a6fa2f725bc715ad
syedenayetali <enayetali2000@gmail.com>
2022-11-12 11:09:39 +0530

    added nodemon and slugify as dependencies

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const replaceTemplate = require("./module/replaceTemplate");
+
 const card = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
   "utf-8"
@@ -17,22 +19,6 @@ const product = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
-
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, product.organic);
-  }
-  return output;
-};
 
 const server = http
   .createServer((req, res) => {
